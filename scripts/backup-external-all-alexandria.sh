6fb9f5f5acbff74242ab753ca8bcc68c08dc179d
Adam Zvolanek <waddoo123+github@gmail.com>
2024-03-13 21:14:25 -0500

    update rsync command with proper flags for exfat filesystem

--- scripts/backup-external-all-alexandria.sh
+++ scripts/backup-external-all-alexandria.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "Running RClone Sync of Unraid Shares to External Drive"
-rsync -avHAX --info=progress2 --exclude {dir}} /mnt/user/ /mnt/disks/{external_drive}/ --log-file={dir}
+echo "Running rsync of Unraid Shares to External Drive"
+rsync -vrltDhH --info=progress2 --exclude={"appdata","transcode","scratch","backups/UniFi_Protect"} "/mnt/user/" "/mnt/disks/WD_Elements_25A3/" --log-file=/mnt/user/backups/Unraid/rsync_backup.log
