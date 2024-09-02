b4faaef7a94b54bce135eca76453c6810ff131b2
Adam Zvolanek <waddoo123+github@gmail.com>
2024-09-02 15:11:25 -0500

    editing photo symlink script

--- scripts/photo_symlink/photo_symlink.bat
+++ scripts/photo_symlink/photo_symlink.bat
@@ -7,13 +7,13 @@ if %errorlevel% neq 0 (
     exit /b 1
 )
 
-REM Prompt user for temporary directory
+REM Prompt user for temporary directory. (e.g. /mnt/user/backups/Adam/Phone\ Backup/2024/TempSpot)
 set /p "temporary_dir=Enter temporary directory: "
 
-REM Prompt user for source directory
+REM Prompt user for source directory (e.g. /mnt/user/backups/Adam/Phone\ Backup/2024/02)
 set /p "source_dir=Enter source directory path: "
 
-REM Prompt user for destination directory
+REM Prompt user for destination directory (e.g. /mnt/user/pictures/Personal/<Year>/<Location>)
 set /p "destination_dir=Enter destination directory path: "
 
 REM Prompt user for password
@@ -23,4 +23,4 @@ REM Replace {alexandria_IP} placeholders with actual IP address or hostname
 set /p "alexandria_IP=Enter Alexandria server IP: "
 
 REM Execute the shell script remotely using plink
-plink -ssh root@%alexandria_IP% -pw %password% "/mnt/user/DevRack/DevRack/scripts/photo_symlink.sh %temporary_dir% %source_dir% %destination_dir%"
+plink -ssh root@%alexandria_IP% -pw %password% "/mnt/user/DevRack/DevRack/scripts/photo_symlink/photo_symlink.sh %temporary_dir% %source_dir% %destination_dir%"
