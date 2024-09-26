6d81844c8f5cc6c9e5e7dfbeab11a42356a56e10
Adam Zvolanek <waddoo123+github@gmail.com>
2024-09-25 21:22:37 -0500

    add server name and secure option to batch script

--- scripts/windows_11/network_drive_mounts.bat
+++ scripts/windows_11/network_drive_mounts.bat
@@ -1,29 +1,62 @@
-echo off
+@echo off
+setlocal
+
+REM Prompt for server name
+set /P server="Enter the server name (e.g., ALEXANDRIA): "
 
 set /P user="Are you Alex or Adam?: "
 
+REM Ask if using secure shares
+set /P secure="Are you using secure shares? (Y/N): "
+
+if /I "%secure%"=="Y" (
+    REM Check if credential already exists
+    cmdkey /list | findstr /I "%server%" >nul
+    if %errorlevel%==0 (
+        echo Credential for %server% already exists. No need to enter again.
+    ) else (
+        REM Prompt for username and password
+        set /P "username=Enter your username: "
+        set /P "password=Enter your password: "
+
+        REM Check if username and password are provided
+        if "%username%"=="" (
+            echo Username cannot be empty. Exiting...
+            exit /b
+        )
+        if "%password%"=="" (
+            echo Password cannot be empty. Exiting...
+            exit /b
+        )
+
+        REM Set Windows Credentials
+        cmdkey /add:%server% /user:%username% /pass:%password%
+    )
+)
+
 REM Common
+net use P: \\%server%\pictures /persistent:yes
+echo Picture share mounted under P
 
-net use P: \\ALEXANDRIA\pictures /persistent:yes
-echo "Picture share mounted under P"
-
-net use V: \\ALEXANDRIA\videos /persistent:yes
-echo "Video share mounted under V"
+net use V: \\%server%\videos /persistent:yes
+echo Video share mounted under V
 
 REM Adam
+if /I "%user%"=="Adam" (
+    net use A: \\%server%\Adam /persistent:yes
+    echo Adam share mounted under A
 
-if %user%==Adam net use A: \\ALEXANDRIA\Adam /persistent:yes
-if %user%==Adam echo "Adam share mounted under A"
+    net use B: \\%server%\DevRack /persistent:yes
+    echo DevRack share mounted under B
 
-if %user%==Adam net use B: \\ALEXANDRIA\DevRack /persistent:yes
-if %user%==Adam echo "DevRack share mounted under B"
-
-if %user%==Adam net use Z: \\ALEXANDRIA\backups /persistent:yes
-if %user%==Adam echo "Backup share mounted under Z"
+    net use Z: \\%server%\backups /persistent:yes
+    echo Backup share mounted under Z
+)
 
 REM Alex
-
-if %user%==Alex net use A: \\ALEXANDRIA\Alex /persistent:yes
-if %user%==Alex echo "Alex share mounted under A"
+if /I "%user%"=="Alex" (
+    net use A: \\%server%\Alex /persistent:yes
+    echo Alex share mounted under A
+)
 
 pause
